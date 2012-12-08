1ded5060788d0ed0388d95353f3e5f1fcdb5afd7
Daniel Petrikin <danpabsolute@gmail.com>
2012-12-07 22:19:49 -0500

    trying different format

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-#!/usr/bin/env python
-
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='djangoappengine_rdbms',
-      version='1.0',
-      description='Django rdbms backend',
-      author='Daniel Petrikin',
-      author_email='dan@blackbeltprogramming.com',
-      url='http://blackbeltprogramming.com',
-      packages=['distutils', 'distutils.command'],
-     )
+    version='0.0.1',
+    description='Django SQL backend',
+    author='Daniel Petrikin',
+    author_email='dan@blackbeltprogramming.com',
+    url='http://github.com/danpetrikin/djangoappengine_rdbms',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
