ef06d7e0f5943eaeb50ba69b9f2042c4d4f4b540
Daniel Petrikin <danpabsolute@gmail.com>
2012-12-07 21:42:29 -0500

    setup py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='djangoappengine_rdbms',
+      version='1.0',
+      description='Django rdbms backend',
+      author='Daniel Petrikin',
+      author_email='dan@blackbeltprogramming.com',
+      url='http://blackbeltprogramming.com',
+      packages=['distutils', 'distutils.command'],
+     )
